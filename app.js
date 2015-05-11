fece16b767d57d06aad02b232f3ebb410e330ea7
Michael Valentine <Michael_Valentine@mentor.com>
2015-05-11 14:52:03 -0700

    Init

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var path = require('path');
+var config = require('konfig')();
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.listen(config.app.port);
+
+
+console.log('Express server running on port '+config.app.port);
+
