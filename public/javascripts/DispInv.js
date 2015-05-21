1818bc60e8da5078ead10e04e34ac023bee1da07
elijah <emv2@pdx.edu>
2015-05-21 09:54:50 -0700

    IM-43

--- public/javascripts/DispInv.js
+++ public/javascripts/DispInv.js
@@ -11,26 +11,24 @@ function DisplayInventory(){
         dispReq=new ActiveXObject("Microsoft.XMLHTTP");
     }
 
-    var host = "http://localhost:50001/displayInventory/";
+    var host = "localhost:50001/displayInventory/";
     dispReq.open('Get', host, true);
     dispReq.send();
     var dispTable = document.getElementById("container");
     dispReq.onreadystatechange = function(){
         if(dispReq.readyState == 4 && dispReq.status==200) {
-            var DisplayInfo = JSON.parse(dispReq.responseText);
+            var DisplayInfo = jQuery.parseJSON(dispReq.responseText);
             var count = DisplayInfo.length;
             for(i= 0; i < count; i++) {
                 var tr =dispTable.insertRow(i);
-                tr.insertCell(0).innerHTML = DisplayInfo[i];
-                tr.append("<td>" + json[i].RunID + "</td>");
-                tr.append("<td>" + json[i].Amount + "</td>");
-                tr.append("<td>" + json[i].Date + "</td>");
-                tr.append("<td>" + json[i].Name + "</td>");
-                tr.append("<td>" + json[i].Customer + "</td>");
-                tr.append("<td>" + json[i].Description + "</td>");
-                tr.append("<td>" + json[i].DateCreated + "</td>");
-                $('table').append(tr);
-                $(table).append("<tr>" + DisplayInfo[i] + "</tr>");
+                tr.insertCell(0).innerHTML = DisplayInfo[i].ProductID;
+                tr.insertCell(1).innerHTML = DisplayInfo[i].RunID;
+                tr.insertCell(2).innerHTML = DisplayInfo[i].Amount;
+                tr.insertCell(3).innerHTML = DisplayInfo[i].Date;
+                tr.insertCell(4).innerHTML = DisplayInfo[i].Name;
+                tr.insertCell(5).innerHTML = DisplayInfo[i].Customer;
+                tr.insertCell(6).innerHTML = DisplayInfo[i].Description;
+                tr.insertCell(7).innerHTML = DisplayInfo[i].DateCreated;
             }
 
         }
