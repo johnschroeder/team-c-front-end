eff1d1b1bbacd4478ba889c1526e223a60acbae5
elijah <emv2@pdx.edu>
2015-05-21 16:44:37 -0700

    IM-43

--- public/javascripts/DispInv.js
+++ public/javascripts/DispInv.js
@@ -6,10 +6,7 @@ function DisplayInventory(){
     {// code for IE7+, Firefox, Chrome, Opera, Safari
         dispReq=new XMLHttpRequest();
     }
-    else
-    {// code for IE6, IE5
-        dispReq=new ActiveXObject("Microsoft.XMLHTTP");
-    }
+   
 
     var host = "http://localhost:50001/displayInventory/";
     dispReq.open('Get', host, true);
