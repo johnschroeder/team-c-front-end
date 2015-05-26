50a729074d59a96d434ed16ab21bf4bb96e3842f
kunmmckeighan <kunmckeighan@gmail.com>
2015-05-26 14:05:05 -0700

    IM-57  DisplayInventory with subbatches

--- public/javascripts/DispInv.js
+++ public/javascripts/DispInv.js
@@ -6,7 +6,7 @@ function DisplayInventory(){
     {// code for IE7+, Firefox, Chrome, Opera, Safari
         dispReq=new XMLHttpRequest();
     }
-   
+
 
     var host = "http://localhost:50001/displayInventory/";
     dispReq.open('Get', host, true);
@@ -15,17 +15,19 @@ function DisplayInventory(){
     dispReq.onreadystatechange = function(){
         if(dispReq.readyState == 4 && dispReq.status==200) {
             var DisplayInfo = jQuery.parseJSON(dispReq.responseText);
-            var count = DisplayInfo.length;
-            for(i= 1; i < count+1; i++) {
-                var tr =dispTable.insertRow(i);
-                tr.insertCell(0).innerHTML = DisplayInfo[i].ProductID;
-                tr.insertCell(1).innerHTML = DisplayInfo[i].RunID;
-                tr.insertCell(2).innerHTML = DisplayInfo[i].Amount;
-                tr.insertCell(3).innerHTML = DisplayInfo[i].Date;
-                tr.insertCell(4).innerHTML = DisplayInfo[i].Name;
-                tr.insertCell(5).innerHTML = DisplayInfo[i].Customer;
-                tr.insertCell(6).innerHTML = DisplayInfo[i].Description;
-                tr.insertCell(7).innerHTML = DisplayInfo[i].DateCreated;
+            var count = DisplayInfo[0].length;
+            for(i=0; i < count; i++) {
+                var tr =dispTable.insertRow(i+1);
+                tr.insertCell(0).innerHTML = DisplayInfo[0][i].ProductID;
+                tr.insertCell(1).innerHTML = DisplayInfo[0][i].RunID;
+                tr.insertCell(2).innerHTML = DisplayInfo[0][i].Amount;
+                var datestr = DisplayInfo[0][i].Date.toString();
+                tr.insertCell(3).innerHTML = datestr.substr(0, datestr.indexOf("T"));
+                tr.insertCell(4).innerHTML = DisplayInfo[0][i].Name;
+                tr.insertCell(5).innerHTML = DisplayInfo[0][i].Customer;
+                tr.insertCell(6).innerHTML = DisplayInfo[0][i].Description;
+                datestr = DisplayInfo[0][i].DateCreated;
+                tr.insertCell(7).innerHTML = datestr.substr(0, datestr.indexOf("T"));
             }
 
         }
