226e57862668df7bacf31d1dc6d32931cd1fade2
Trevor <rtrevor@pdx.edu>
2015-05-29 16:48:56 -0700

    Another commit for logs.

--- public/javascripts/DispLogs.js
+++ public/javascripts/DispLogs.js
@@ -14,14 +14,27 @@ function DisplayLogs() {
     var dispTable = document.getElementById("container");
 
     dispReq.onreadystatechange = function () {
-        if (dispReq.readyState == 4 && dispReq.status == 200) {
+        if (!(dispReq.readyState == 4 && dispReq.status == 200)) {
+        } else {
             var DisplayInfo = jQuery.parseJSON(dispReq.responseText);
             var count = DisplayInfo.length;
             for (i = 0; i < count; i++) {
+                var di = DisplayInfo[i];
                 var tr = dispTable.insertRow(i + 1);
                 tr.insertCell(0).innerHTML = "<input type='checkbox' name= 'logMark' + i value='logNumber' + i>"
-                tr.insertCell(1).innerHTML = DisplayInfo[i].LogID;
-                tr.insertCell(2).innerHTML = DisplayInfo[i].productID;
+                switch (di.LogType) {
+                    case 0:
+                        tr.insertCell(1).innerHTML = di.date + " " + di.amount + " " +
+                            di.name + " added to " + di.customerID + " inventory";
+                        break;
+                    case 1:
+                        tr.insertCell(1).innerHTML = di.date + " " + di.customerID + " audited details";
+                        break;
+                    case 2:
+                        tr.insertCell(1).innerHTML = di.UserName + " left a transaction note on " + di.name;
+                        break;
+                }
+
             }
 
         }
