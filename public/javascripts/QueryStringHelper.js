15bb823c70cc3eba3baeb003f0e9f5a64c9fec99
hinyunlee <hinyunlee@gmail.com>
2015-05-28 21:38:00 -0700

    IM-52: Refactored query string and added buttons

---
+++ public/javascripts/QueryStringHelper.js
@@ -0,0 +1,29 @@
+function getQueryStringParams() {
+    var params = {};
+    location.search.substr(1).split("&").forEach(function(each) {
+        if (each !== "") {
+            var pair = each.split("=");
+            params[pair[0]] = pair[1];
+        }
+    });
+
+    return params;
+}
+
+function mapToQueryString(map) {
+    var s = "";
+    var entries = [];
+
+    for (var k in map) {
+        entries.push(k + "=" + map[k]);
+    }
+
+    for (var i = 0; i < entries.length; ++i) {
+        s += entries[i];
+        if (i < entries.length - 1) {
+            s += "&";
+        }
+    }
+
+    return s;
+}
