8c4a2d30c344b61560a1a6a2d1ac1119942e51ea
Trevor <rtrevor@pdx.edu>
2015-05-31 19:01:56 -0700

    Backend seems to work except for the weird filter bug.

--- public/javascripts/DispLogs.js
+++ public/javascripts/DispLogs.js
@@ -1,4 +1,4 @@
-function DisplayLogs() {
+function DisplayLogs(afterdate) {
 
     document.getElementById("errorMsg").innerHTML = "";
 
@@ -6,12 +6,25 @@ function DisplayLogs() {
         dispReq = new XMLHttpRequest();
     }
 
-
-    var host = "http://localhost:50001/displayLogs/";
+    var after = '00-00-0000';
+    var host = "http://localhost:50001/displayLogs/" + afterdate + "/";
     dispReq.open('Get', host, true);
     dispReq.send();
 
     var dispTable = document.getElementById("container");
+    var tableRows = dispTable.getElementsByTagName('tr');
+    var rowCount = tableRows.length;
+    // Removes the old static table so a new one can be added.
+    for (var x = rowCount - 1; x >= 0; x--) {
+        document.getElementById("container").deleteRow(x);
+    }
+
+    var row = dispTable.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "SELECT";
+    cell2.innerHTML = "Log Message";
+
     dispReq.onreadystatechange = function () {
         if (!(dispReq.readyState == 4 && dispReq.status == 200)) {
         } else {
@@ -21,25 +34,39 @@ function DisplayLogs() {
                 var di = DisplayInfo[i];
                 var tr = dispTable.insertRow(i + 1);
                 var value = '' + di.logID;
-                var checkbox = document.createElement("input");
 
-                checkbox.type = "checkbox";    // make the element a checkbox
-                checkbox.name = "logMark";      // give it a name we can check on the server side
-                checkbox.value = value;         // make its value "pair"
+                var checkbox = document.createElement('input');
+                checkbox.type = "checkbox";
+                checkbox.name = "logMark";
+                checkbox.value = value;
+                checkbox.id = "id";
 
-                tr.insertCell(0).innerHTML.checkbox;
+                var label = document.createElement('label')
+                label.htmlFor = "id";
+                var logMessage = "";
+
                 switch (di.LogType) {
                     case 0:
-                        tr.insertCell(1).innerHTML = di.date + " " + di.amount + " " +
+                        logMessage = di.date + " " + di.amount + " " +
                             di.name + " added to " + di.customerID + " inventory";
                         break;
                     case 1:
-                        tr.insertCell(1).innerHTML = di.date + " " + di.customerID + " audited details";
+                        logMessage = di.date + " " + di.customerID + " audited details";
                         break;
                     case 2:
-                        tr.insertCell(1).innerHTML = di.UserName + " left a transaction note on " + di.name;
+                        logMessage = di.UserName + " left a transaction note on " + di.name;
                         break;
                 }
+
+                label.appendChild(document.createTextNode(logMessage));
+
+                row = dispTable.insertRow(i + 1);
+
+                cell1 = row.insertCell(0);
+                cell2 = row.insertCell(1);
+
+                cell1.appendChild(checkbox);
+                cell2.appendChild(label);
 
             }
 
