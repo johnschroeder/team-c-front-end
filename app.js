df3eb84fd1cafb79a35cabe5e3e593f39da2e84a
Michael Valentine <Michael_Valentine@mentor.com>
2015-06-11 14:49:15 -0700

    ouch that was painful

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var path = require('path');
 var config = require('konfig')();
-
 var app = express();
+var apiRoute = config.app.api;
+var ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
+
+app.get("/", function(req, res){
+    res.render("indexHeader.ejs", {apiRoute: apiRoute});
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
+
 
 
 // catch 404 and forward to error handler
@@ -15,27 +24,6 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 
 app.listen(config.app.port);
 
