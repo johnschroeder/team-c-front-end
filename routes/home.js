8168b5d695b5d0d611abeed5505e8a1b05906127
Michael Valentine <Michael_Valentine@mentor.com>
2015-06-16 13:51:10 -0700

    Changing route to post request

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,13 @@
+/**
+ * Created by js22 on 6/16/15.
+ */
 var express = require("express");
 var router = express.Router();
+var Q = require('q');
 
 
-router.route("/").post(function(req,res){
+router.route("/home").post(function(req,res){
     res.render("Home.html", req.body);
 });
-
 
 module.exports = router;
