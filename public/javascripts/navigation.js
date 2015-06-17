52d5d91b23aa357f68a87aca781b126b922331aa
johnschroeder <john.schroeder4@gmail.com>
2015-06-17 15:25:52 -0700

    IM-69 minor changes

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -1,9 +1,9 @@
 var navigation = {
-    stateArray:{},
+    stateTable:{},
     go:function(targetPage, args) {
-        $("#main_cont").load('/load/' + targetPage, {args:args, state:this.stateArray[targetPage.toLowerCase().split(".")[0]]});
+        $("#main_cont").load('/load/' + targetPage, {args:args, state:this.stateTable[targetPage.toLowerCase().split(".")[0]]});
     },
     saveState:function(state) {
-        this.stateArray[window.thisPage.toLowerCase().split(".")[0]] = state;
+        this.stateTable[window.thisPage.toLowerCase().split(".")[0]] = state;
     }
 }
