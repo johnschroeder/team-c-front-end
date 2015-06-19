aba1c467a370c21dcfcb315650d4291e22eb0aaf
kunmmckeighan <kunmckeighan@gmail.com>
2015-06-18 17:41:44 -0700

    IM-68 move js to seperate DisplayInventory.js

--- public/javascripts/DisplayInventory.js
+++ public/javascripts/DisplayInventory.js
@@ -2,12 +2,11 @@
  * Created by Kun on 6/18/2015.
  */
 
-function gotoPullInventory(pid)
+function gotoPullInventory(pid,pname,tlq)
 {
-    //alert("ddddddddddddd");
     navigation.go("PullInventory.html",
         { ProductID: pid,
-        //ProductName: pname,
-        //TotalQuantity: ttlq,
+        ProductName: pname,
+        TotalQuantity: tlq,
         PreviousPage: "DisplayInventory.html"});
 }
--- public/javascripts/PullInventory.js
+++ public/javascripts/PullInventory.js
@@ -5,5 +5,11 @@
 
 function init() {
     var productID = window.args.ProductID;
+    var productName = window.args.ProductName;
+    var previousPage = window.args.PreviousPage;
+    var totalQuantity = window.args.TotalQuantity;
     console.log("PRODUCT ID: " + productID);
+    console.log("PRODUCT Name: " + productName);
+    console.log("Total Quantity: " + totalQuantity);
+    console.log("Previous Page" + previousPage);
 }
