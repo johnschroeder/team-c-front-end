f8ed299fd17ba776bf2c174e7c8007bd4e28aa65
Trevor <rtrevor@pdx.edu>
2015-06-23 15:07:40 -0700

    This should work, except I don't have the new stuff addeded like 'state', 'navigation', '(window or config?) arguments passed'

--- public/javascripts/EditCart.js
+++ public/javascripts/EditCart.js
@@ -17,6 +17,16 @@ function showProductID(selectedID) {
     });
 }
 
+function getID() {
+
+    var cartIDDiv = document.getElementById('cart_id');
+    var id = 'replace me'; // window.args.cartID;
+    cartIDDiv.innerHTML = id;
+}
+
+function myCartID() {
+    return '2';
+}
 
 function sendRequest(host, callback) {
     if (window.XMLHttpRequest) {
@@ -27,15 +37,14 @@ function sendRequest(host, callback) {
     dispReq.onreadystatechange = callback;
 }
 
-function CartEdit(selectedID) {
+function CartEdit(cartID) {
     //TODO implement: var prodID = getQueryStringParams().inventoryID; and get the prodID from there
 
-    var cartID = selectedID;
     var newCartName = $(".CartName").val();
     var newReporter = $(".Reporter").val();
     var newAssignee = $(".Assignee").val();
     var newDate = $(".Date").val();
-    var host = 'http://localhost:50001/Carts/EditCart/' + '2' + '/' + '"' + newCartName + '"' + "/" + '"' + newReporter + '"' + "/" + '"' + newAssignee + '"' + "/" + '"' + newDate + '"';
+    var host = 'http://localhost:50001/Carts/EditCart/' + cartID + '/' + '"' + newCartName + '"' + "/" + '"' + newReporter + '"' + "/" + '"' + newAssignee + '"' + "/" + '"' + newDate + '"';
     alert(host);
 
     sendRequest(host, function () {
@@ -45,10 +54,6 @@ function CartEdit(selectedID) {
     });
 }
 
-
-
-
-
-
-
-
+function CartItemsEdit(selectedID) {
+    alert("Navigates to John's page");
+}
