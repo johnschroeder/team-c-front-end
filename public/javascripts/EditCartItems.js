b58916ed6755185af923a5d7d57ebdfdcba81bae
johnschroeder <js22@pdx.edu>
2015-06-23 15:53:12 -0700

    IM-79 Hopefully this is my only commit to change the backend

--- public/javascripts/EditCartItems.js
+++ public/javascripts/EditCartItems.js
@@ -9,7 +9,6 @@ var editCartItems = {
         $.get(window.apiRoute + "/Carts/GetCartItems/" + this.cartID, function(res) {
             if (res && res.length) {
                 editCartItems.cartItems = JSON.parse(res)[0];
-                $("#cart_name").text(editCartItems.cartName);
                 editCartItems.populateList();
             } else {
                 $("#response").text("Error: EditCartItems.init: No response.");
@@ -18,30 +17,61 @@ var editCartItems = {
             $("#response").text("Error: EditCartItems.init: Connection error.");
         });
     },
+
+    //TODO: any fucking comments
     populateList: function() {
+        $("#cart_name").text(this.cartName);
         var inventory_container = $( '.inventory-container' );
         var itemList = $(document.createElement("div"))
             .appendTo(inventory_container);
         console.log("Cart items: " + this.cartItems);
 
+        var productTable = [];
         for(var i = 0; i < this.cartItems.length; ++i) {
-            var name = this.cartItems[i].ProductName.toString();
-            var total = this.cartItems[i].Total.toString();
+            var index = this.cartItems[i].ProductID;
+            console.log("index: " + index);
 
+            if(typeof productTable[index] === 'undefined') {
+                productTable[index] = [];
+                productTable[index].push({
+                    name: this.cartItems[i].ProductName,
+                    totalQuantity: 0,
+                    productID: index
+                });
+            }
+            productTable[index].push(this.cartItems[i]);
+            productTable[index][0].totalQuantity += this.cartItems[i].Total;
+        }
+
+        console.log(productTable);
+
+        productTable.forEach(function(product) {
             var cartItem = $(document.createElement("div"))
                 .appendTo(itemList);
-            $(document.createElement("hr"))
-                .appendTo(cartItem);
+            $(document.createElement("hr")).appendTo(cartItem);
             var productName = $(document.createElement("span"))
-                .text(name)
+                .text(product[0].name)
                 .appendTo(cartItem);
             var totalQuantity = $(document.createElement("span"))
-                .text(total)
+                .text(product[0].totalQuantity)
                 .addClass("float_right")
                 .appendTo(cartItem);
+            $(document.createElement("br")).appendTo(cartItem);
+            var editButton = $(document.createElement("button"))
+                .text("Edit")
+                .attr("onclick", 'editCartItems.editItem()');
+            editButton.appendTo(cartItem);
+            var optionsContainer = $(document.createElement("div"))
+                .appendTo(cartItem);
+            for(var i = 1; i < product.length; ++i) {
+                var size = $(document.createElement("span"))
+                    .text(product[i].SizeName)
+                    .appendTo(cartItem);
+            }
             itemList.append(cartItem);
-
-        }
+        })
         inventory_container.append(itemList);
-    }
+    },
+
+    editItem: function() {alert("it worked");}
 }
