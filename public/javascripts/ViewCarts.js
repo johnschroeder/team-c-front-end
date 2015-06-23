96dd2d90732307c27ae9604f163f7932dfd63ea7
elijah <emv2@pdx.edu>
2015-06-23 01:12:43 -0700

    done for the night, attempting to dynamiclly add a div and then select html tag into the html using javascript, seem to have failed miserably... :(

--- public/javascripts/ViewCarts.js
+++ public/javascripts/ViewCarts.js
@@ -2,11 +2,24 @@
  * Created by elijah on 6/21/15.
  */
 function populateByCartId(){
+
     $.ajax({
-        url:"http://localhost:50001/Carts/GetCartsByUser/test01/",
+        url:window.apiRoute + "/Carts/GetCartsByUser/don",
         type:"Get",
         async:true,
-        success:function(result){}
+        success:function(result){
+            var dropDownDiv = document.getelementbyID("selectDropDown");
+            var dropSelect  = document.createElement("Select")
+            var i;
+            var cId = $.parseJSON(result);
+            for(var i = 0; i < result.length; i++ ){
+                var option = document.createElement("option");
+                option.value = i;
+                option.text = cId[i].CartID;
+                dropSelect.option.add(option);
+            }
+            console.log(cId);
+            dropDownDiv.appendChild(dropSelect);
+        }
     });
-    console.log(result);
 }
