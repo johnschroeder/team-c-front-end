6d389e546887da250a4bc11cd04b3fd34e2192eb
Trevor <rtrevor@pdx.edu>
2015-06-24 15:17:24 -0700

    Ok. I think it's ready for review again. Replaced stubbed data with a route hit.

--- public/javascripts/EditCartData.js
+++ public/javascripts/EditCartData.js
@@ -1,21 +1,12 @@
-function showProductID(selectedID) {
-    //TODO use john's breadcrumb loader to load a new page here populated with the data.
-    console.log(selectedID);
-    var host = "http://localhost:50001/EditProduct/" + selectedID + "/";
-
-    sendRequest(host, function () {
-        if (dispReq.readyState == 4 && dispReq.status == 200) {
-            var selectedProduct = jQuery.parseJSON(dispReq.responseText);
-            $(".selectedID").text(selectedProduct[0].ProductID);
-            //fill Customer
-            $(".customer").text(selectedProduct[0].Customer);
-            //fill Item Name
-            $(".product-name").text(selectedProduct[0].Name);
-            //fill Description
-            $(".description").text(selectedProduct[0].Description);
-        }
-    });
+function sendRequest(host, callback) {
+    if (window.XMLHttpRequest) {
+        dispReq = new XMLHttpRequest();
+    }
+    dispReq.open("Get", host, true);
+    dispReq.send();
+    dispReq.onreadystatechange = callback
 }
+
 
 
 function setCurrentValues() {
@@ -23,35 +14,42 @@ function setCurrentValues() {
     var original_name_div = document.getElementById('original_cart_name');
     var name = window.args.cartName;
     original_name_div.innerHTML = "Editing Cart: " + name;
+    var host = "http://localhost:50001/Carts/GetAllCarts/"
+    var carts;
+    var cartIndex = 0;
 
-    /* var host = 'http://localhost:50001/Carts/GetMyCartData/' + cartID + '/';
-     sendRequest(host, function () {
-     if (dispReq.readyState == 4 && dispReq.status == 200) {
-     console.log("Success!");
-     }
-     });
-     */
+    var original_cart = "original_cart";
+    var original_reporter = "original_reporter";
+    var original_assignee = "original_assignee";
+    var original_date = "original_date";
 
 
-    var newCartName = $(".CartName").val("orignal_cart");
-    var newReporter = $(".Reporter").val("orignal_reporter");
-    var newAssignee = $(".Assignee").val("original_Assignee");
-    $(".Date").val("orignal_date");
+    $.get(host, function (data, status) {
+        carts = jQuery.parseJSON(data);
+        for (var i = 0; i < carts.length; i++) {
+            if (carts[i].CartID == window.args.cartID) {
+                cartIndex = i;
+            }
+        }
+
+        original_cart = carts[cartIndex].CartName;
+        original_reporter = carts[cartIndex].Reporter;
+        original_assignee = carts[cartIndex].Assignee;
+        original_date = carts[cartIndex].DateToDelete;
+
+        var newCartName = $(".CartName").val(original_cart);
+        var newReporter = $(".Reporter").val(original_reporter);
+        var newAssignee = $(".Assignee").val(original_assignee);
+        var newDate = $(".Date").val(original_date.split("T")[0]);
+    });
+
+
 }
 
 function myCartID() {
     return window.args.cartID;
 }
 
-
-function sendRequest(host, callback) {
-    if (window.XMLHttpRequest) {
-        dispReq = new XMLHttpRequest();
-    }
-    dispReq.open("Get", host, true);
-    dispReq.send();
-    dispReq.onreadystatechange = callback;
-}
 
 function CartDataEdit(cartID) {
     //TODO implement: var prodID = getQueryStringParams().inventoryID; and get the prodID from there
