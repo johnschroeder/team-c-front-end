06e8bd385a04a147edade7ee77b72471648f435c
elijah <emv2@pdx.edu>
2015-06-24 21:33:59 -0700

    connects to trevs page

--- public/javascripts/ViewCarts.js
+++ public/javascripts/ViewCarts.js
@@ -3,38 +3,23 @@
  */
 var state = window.state;
 function populateByCartId(){
-
+    //TODO when users is able to be gotten dynamicly, change "don" to + userid; so it grabs the carts for the user
     $.get(window.apiRoute + "/Carts/GetCartsByUser/don", function(res) {
         if(res && res.length) {
             var dropSelect = document.getElementById("selectDropDown");
             var results = JSON.parse(res)[0];
-            var option1 =document.createElement("option");
-            option1.text = 999;
-            dropSelect.appendChild(option1);
-            var option2 =document.createElement("option");
-            option2.text = 42;
-            dropSelect.appendChild(option2);
-            var option3 =document.createElement("option");
-            option3.text = 33;
-            dropSelect.appendChild(option3);
 
             for(var i = 0; i < results.length; i++ ) {
                 var option = document.createElement("option");
                 option.value = results[i].CartID;
-  /*              if(!(typeof state === 'undefined'))
-                if(results[i].CartName == window.state.nameSelected){
-                    option.attr("selected");
+                if(!(typeof state === 'undefined')) {
+                    if (results[i].CartName == state.nameSelected) {
+                        option.attr = ("selected");
+                    }
                 }
-    */            option.text = results[i].CartName;
+                option.text = results[i].CartName;
                 dropSelect.add(option);
             }
-          /*  if(typeof state.nameSelected === 'undefined'){
-            }
-            else{
-                var option = $(document.createElement("option"))
-                    .attr("selected");
-                dropSelect = state.nameSelected;
-            }*/
         }
         else {
                 $("#response").text("Error: Init: No response.");
@@ -45,43 +30,20 @@ function populateByCartId(){
 }
 
 function displayCartInventory(){
-
+    $(".inventory-container").empty();
     var cartContainer = $(".inventory-container");
     var cartList = $(document.createElement("div"))
         .appendTo(cartContainer);
+    console.log("nameselected:" + $("#selectDropDown :selected").text());
 
-//    state.nameSelected = $("#selectDropDown :selected").text();
-//    navigation.saveState(window.state);
+    state.nameSelected =$("#selectDropDown :selected").text();
+    navigation.saveState(state);
     var idSelected = $("#selectDropDown :selected").value;
 
     $.get(window.apiRoute + "/Carts/GetCartItems/" + idSelected, function(res) {
         if (res && res.length) {
             var items = JSON.parse(res)[0];
-            for(var i = 0; i < items.length; i++){
-                var name = items[i].ProductName.toString();
-                var total = items[i].Total.toString();
-                var sName  = items[i].SizeName.toString() + " of " + items[i].CountPerBatch.toString() + " * " + items[i].BatchCount.toString();
-
-                var cartItem = $(document.createElement("div"))
-                    .appendTo(cartList);
-                $(document.createElement("hr"))
-                    .appendTo(cartItem);
-                var productName = $(document.createElement("span"))
-                    .text(name)
-                    .appendTo(cartItem);
-                var total = $(document.createElement("span"))
-                    .text(total)
-                    .addClass("float_right")
-                    .appendTo(cartItem);
-                $(document.createElement("br"))
-                    .appendTo(cartItem);
-                var sizeName = $(document.createElement("span"))
-                    .text(sName)
-                    .appendTo(cartItem);
-                cartList.append(cartItem);
-            }
-            cartContainer.append(cartList);
-
+            populateCartContainer(items);
         }
         else {
             $("#response").text("Error: Init: No response.");
@@ -91,13 +53,57 @@ function displayCartInventory(){
     });
 }
 
-/*function populateCartContainer(){
-    for(var i = 0; i < cartStuff.length; i++){
-        var productName = $(document.createElement("span"))
-            .attr= cartStuff[i].Name;
-        var cartItem = $(document.createElement("div"))
-            .appendTo(cartList);
+function gotoEditCarts(){
+    if(typeof state === 'undefined'){
+        $('#notSelected').text("Please choose a cart first!")
+    }
+    else{
+        var cartSelected = $("#selectDropDown :selected").text();
+        state.nameSelected =$("#selectDropDown :selected").text();
+        navigation.saveState(state);
+        var idSelected = $("#selectDropDown :selected").value;
+        navigation.go('EditCartData.html',{cartID:idSelected, cartName:cartSelected});
 
     }
+}
 
-}*/
+function populateCartContainer(items){
+    $(".inventory-container").empty();
+    var cartContainer = $(".inventory-container");
+    var cartList = $(document.createElement("div"))
+        .appendTo(cartContainer);
+    for(var i = 0; i < items.length; i++){
+        var name = items[i].ProductName.toString();
+        var total = items[i].Total.toString();
+        var sName  = items[i].SizeName.toString() + " of " + items[i].CountPerBatch.toString() + " * " + items[i].BatchCount.toString()+  " = " + total;
+        var color = "Run Color/Marker  " + items[i].Marker.toString();
+        var location = "Location:  "+ items[i].Location.toString();
+
+        var cartItem = $(document.createElement("div"))
+            .appendTo(cartList);
+        $(document.createElement("hr"))
+            .appendTo(cartItem);
+        var productName = $(document.createElement("span"))
+            .text(name)
+            .addClass("productName")
+            .appendTo(cartItem);
+        var total = $(document.createElement("span"))
+            .text(total)
+            .addClass("float_right")
+            .appendTo(cartItem);
+        $(document.createElement("br"))
+            .appendTo(cartItem);
+        var sizeName = $(document.createElement("span"))
+            .text(sName)
+            .appendTo(cartItem);
+        var runMarker = $(document.createElement("span"))
+            .text(color)
+            .appendTo(cartItem);
+        var ilocation = $(document.createElement("span"))
+            .text(location)
+            .addClass("float_right")
+            .appendTo(cartItem);
+        cartList.append(cartItem);
+    }
+    cartContainer.append(cartList);
+}
