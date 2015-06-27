3fbac7e93c4cf76715ecc51a3c586cf9546d13be
hinyunlee <hinyunlee@gmail.com>
2015-06-27 16:28:18 -0700

    IM-91: Major rework on inventory pages
    Much work done on display inventory and the pages it relates to.

--- public/javascripts/EditProduct.js
+++ public/javascripts/EditProduct.js
@@ -1,47 +1,29 @@
-function showProductID(selectedID){
-    //TODO use john's breadcrumb loader to load a new page here populated with the data.
-    console.log(selectedID);
-    var host = "http://localhost:50001/EditProduct/" +selectedID +"/";
+var editProduct = {
+    product: null,
+    init: function() {
+        //TODO use john's breadcrumb loader to load a new page here populated with the data.
 
-    sendRequest(host, function() {
-        if(dispReq.readyState == 4 && dispReq.status == 200) {
-            var selectedProduct = jQuery.parseJSON(dispReq.responseText);
-            $(".selectedID").text(selectedProduct[0].ProductID);
-            //fill Customer
-            $(".customer").text(selectedProduct[0].Customer);
-            //fill Item Name
-            $(".product-name").text(selectedProduct[0].Name);
-            //fill Description
-            $(".description").text(selectedProduct[0].Description);
-        }
-    });
-}
+        var self = this;
 
+        $.get(window.apiRoute + "/EditProduct/" + window.args.ProductID , function(res) {
+            self.product = $.parseJSON(res)[0];
+            $(".selectedID").text(self.product.ProductID);
+            $(".customer").text(self.product.Customer); //fill Customer
+            $(".product-name").text(self.product.Name); //fill Item Name
+            $(".description").text(self.product.Description); //fill Description
+        });
+    },
 
-function sendRequest(host, callback) {
-    if (window.XMLHttpRequest) {
-        dispReq = new XMLHttpRequest();
-    }
-    dispReq.open("Get", host, true);
-    dispReq.send();
-    dispReq.onreadystatechange = callback;
-}
+    reSubmit: function() {
+        var newCustName = $(".customer").val();
+        var newProdName  = $(".product-name").val();
+        var newDescript  = $(".description").val();
 
-function reSubmit(selectedID){
-    //TODO implement: var prodID = getQueryStringParams().inventoryID; and get the prodID from there
-    var prodID = selectedID;
-    var newCustName = $(".customer").val();
-    var newProdName  = $(".product-name").val();
-    var newDescript  = $(".description").val();
-    var host = "http://localhost:50001/reSubmit/" + prodID + "/" + newCustName +"/" + newProdName + "/" + newDescript;
-
-    sendRequest(host, function() {
-        if (dispReq.readyState == 4 && dispReq.status == 200) {
+        $.get(window.apiRoute + "/reSubmit/" + self.product.ProductId + "/" + newCustName +"/" + newProdName + "/" + newDescript, function() {
             console.log("Success!");
-        }
-    });
-}
-
+        });
+    }
+};
 
 
 
