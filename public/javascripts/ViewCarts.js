7448fb7bd1baa27834e34ec6467c809a42eeb2af
hinyunlee <hinyunlee@gmail.com>
2015-06-30 01:11:08 -0700

    IM-91: Final commit for code review

--- public/javascripts/ViewCarts.js
+++ public/javascripts/ViewCarts.js
@@ -1,19 +1,19 @@
+//<script>
 /**
  * Created by elijah on 6/21/15.
  */
 
 var state = window.state;
-//<script>
+
 function populateByCartId() {
     //TODO when function runs we need to make sure that if there is a state.nameSelected that it is put in the selected option on run.
-    //TODO when users is able to be gotten dynamicly, change "don" to + userid; so it grabs the carts for the user
+    //TODO when users is able to be gotten dynamically, change "don" to + userid; so it grabs the carts for the user
     var user = 'don';
     $.get(window.apiRoute + "/Carts/GetCartsByUser/" + user, function(res) {
         if (res && res.length) {
-            var dropSelect = $("#selectDropDown");
             var results = JSON.parse(res);
 
-            dropSelect
+            var dropSelect = $("#selectDropDown")
                 .append($("<option/>")
                     .val(-1)
                     .text("-- Select a Cart --")
@@ -45,10 +45,7 @@ function displayCartInventory() {
 
     if ($("#selectDropDown :selected").val() == -1) return;
 
-    var cartList = $("<div/>")
-        .appendTo(cartContainer);
-
-    console.log("Selected: " + $("#selectDropDown :selected").text());
+    //console.log("Selected: " + $("#selectDropDown :selected").text());
 
     state.nameSelected = $("#selectDropDown :selected").text();
     navigation.saveState(state);
@@ -105,43 +102,7 @@ function populateCartContainer(items) {
 
     for (var i = 0; i < items.length; ++i) {
         if (!cartItem || cartItem.find(".item-name").text() != items[i].ProductName) {
-            cartItem = $("<div/>")
-                .addClass("cart-item")
-                .append($("<hr/>"))
-                .append($("<div/>")
-                    .addClass("row")
-                    .append($("<div/>")
-                        .addClass("col-sm-8")
-                        .append($("<h2/>")
-                            .addClass("item-name")
-                            .text(items[i].ProductName)
-                        )
-                    )
-                    .append($("<div/>")
-                        .addClass("col-sm-4")
-                        .append($("<h2/>")
-                            .addClass("item-total-text text-center")
-                            .text("0")
-                        )
-                    )
-                )
-                .append($("<div/>")
-                    .addClass("row")
-                    .append($("<div/>")
-                        .addClass("col-sm-6")
-                        .text("Pulled")
-                    )
-                    .append($("<div/>")
-                        .addClass("col-sm-6")
-                        .append($("<button/>")
-                            .addClass("btn btn-default pull-right")
-                            .click(function() { /* TODO Unpull() */ })
-                            .text("Unpull")
-                        )
-                    )
-                )
-                .append($("<br/>"))
-                .appendTo(cartContainer);
+            cartItem = createCartEntry(items[i], cartContainer);
         }
 
         var entry = createItemEntry(items[i], cartItem)
@@ -151,19 +112,131 @@ function populateCartContainer(items) {
     }
 }
 
+function createCartEntry(item, parent) {
+    var cartItem = $("<div/>");
+
+    cartItem
+        .addClass("cart-item")
+        .data("name", item.ProductName)
+        .data("product_id", item.ProductID)
+        .append($("<hr/>"))
+        .append($("<div/>")
+            .addClass("row")
+            .append($("<div/>")
+                .addClass("col-sm-8")
+                .append($("<h2/>")
+                    .addClass("item-name")
+                    .text(item.ProductName)
+                )
+            )
+            .append($("<div/>")
+                .addClass("col-sm-4")
+                .append($("<h2/>")
+                    .addClass("item-total-text text-center")
+                    .text("0")
+                )
+            )
+        )
+        .append($("<div/>")
+            .addClass("row")
+            .append($("<div/>")
+                .addClass("col-sm-6")
+                .text("Pulled")
+            )
+            .append($("<div/>")
+                .addClass("col-sm-6")
+                .append($("<div/>")
+                    .addClass("pull-right")
+                    .append($("<button/>")
+                        .addClass("btn btn-default hidden edit-button")
+                        .click(function() {
+                            cartItem
+                                .find(".item-input")
+                                    .data("dirty", false)
+                                    .find(".package-text")
+                                       .addClass("hidden")
+                                    .end()
+                                    .find(".amount-text")
+                                       .addClass("hidden")
+                                    .end()
+                                    .find(".package-select")
+                                        .removeClass("hidden")
+                                    .end()
+                                    .find(".amount-input")
+                                        .removeClass("hidden")
+                                    .end()
+                                .end()
+                                .find(".edit-button")
+                                   .addClass("hidden")
+                                .end()
+                                .find(".done-button")
+                                    .removeClass("hidden")
+                                .end();
+
+                            updateEntryPackageTypeOptions(cartItem.find(".item-input"), item.ProductID)
+                        })
+                        .text("Edit")
+                    )
+                    .append($("<button/>")
+                        .addClass("btn btn-default hidden done-button")
+                        .click(function() {
+                            cartItem
+                                .find(".item-input")
+                                    .find(".package-text")
+                                       .removeClass("hidden")
+                                    .end()
+                                    .find(".amount-text")
+                                       .removeClass("hidden")
+                                    .end()
+                                    .find(".package-select")
+                                        .addClass("hidden")
+                                    .end()
+                                    .find(".amount-input")
+                                        .addClass("hidden")
+                                    .end()
+                                .end()
+                                .find(".edit-button")
+                                   .removeClass("hidden")
+                                .end()
+                                .find(".done-button")
+                                    .addClass("hidden")
+                                .end();
+
+                            handleDirtyInput(cartItem);
+                        })
+                        .text("Done")
+                    )
+                    .append($("<button/>")
+                        .addClass("btn btn-default hidden delete-button")
+                        .click(function() {})
+                        .text("Delete")
+                    )
+                    .append($("<button/>")
+                        .addClass("btn btn-default")
+                        .click(function() { /* TODO Unpull() */ })
+                        .text("Unpull")
+                    )
+                )
+            )
+        )
+        .append($("<br/>"))
+        .appendTo(parent);
+
+        return cartItem;
+}
+
 function createItemEntry(item, parent) {
-    var name = item.ProductName;
     var total = parseInt(item.UnitsPerPackage) * parseInt(item.NumPackages);
     var sName  = item.PackageName + " of " + item.UnitsPerPackage + " * " + item.NumPackages + " = " + total;
-    var color = "Run Color/Marker: " + item.Marker;
-    var location = "Location: "+ item.Location;
+    var color = "Marker: " + (item.Marker || "");
+    var location = "Location: "+ (item.Location || "");
 
     var inputDiv = $("<div/>");
 
-    var cartItem = $("<div/>")
-        .addClass("row")
+    var cartItemEntry = $("<div/>")
+        .addClass("row item-entry")
         .append(inputDiv
-            .addClass("col-sm-7 form-group input-div")
+            .addClass("col-sm-8 form-group item-input")
             .append($("<div/>")
                 .addClass("row")
                 .append($("<div/>")
@@ -178,7 +251,7 @@ function createItemEntry(item, parent) {
                         .addClass("form-control hidden package-select")
                         .change(function() {
                             var selected = inputDiv.find(".package-select option:selected");
-                            cartItem.find(".total-text").text(parseInt(selected.data("size")) * parseInt(inputDiv.find(".amount-input").val()));
+                            cartItemEntry.find(".total-text").text(parseInt(selected.data("size")) * parseInt(inputDiv.find(".amount-input").val()));
 
                             inputDiv.find(".package-text")
                                 .attr("name", selected.attr("name"))
@@ -191,8 +264,9 @@ function createItemEntry(item, parent) {
                     )
                 )
                 .append($("<div/>")
-                    .addClass("col-sm-1")
-                    .text(" * ")
+                    .addClass("col-sm-1 text-center")
+                    .css("font-size", "150%")
+                    .text("*")
                 )
                 .append($("<div/>")
                     .addClass("col-sm-3")
@@ -207,7 +281,7 @@ function createItemEntry(item, parent) {
                         .val(parseInt(item.NumPackages))
                         .change(function() {
                             var selected = inputDiv.find(".package-select option:selected");
-                            cartItem.find(".total-text").text(parseInt(selected.data("size")) * parseInt(inputDiv.find(".amount-input").val()));
+                            cartItemEntry.find(".total-text").text(parseInt(selected.data("size")) * parseInt(inputDiv.find(".amount-input").val()));
 
                             inputDiv.find(".amount-text")
                                 .text(inputDiv.find(".amount-input").val());
@@ -218,12 +292,13 @@ function createItemEntry(item, parent) {
                     )
                 )
                 .append($("<div/>")
-                    .addClass("col-sm-3")
-                    .text(" = ")
-                    .append($("<span/>")
-                        .addClass("total-text")
-                        .text(total)
-                    )
+                    .addClass("col-sm-1 text-center")
+                    .text("=")
+
+                )
+                .append($("<div/>")
+                    .addClass("col-sm-2 total-text")
+                    .text(total)
                 )
             )
         )
@@ -235,102 +310,47 @@ function createItemEntry(item, parent) {
             .addClass("col-sm-2")
             .text(location)
         )
-        .append($("<div/>")
-            .addClass("col-sm-1")
-            .append($("<button/>")
-                .addClass("btn btn-default pull-right edit-button")
-                .click(function() {
-                    inputDiv
-                        .data("dirty", false)
-                        .find(".package-text")
-                           .addClass("hidden")
-                        .end()
-                        .find(".amount-text")
-                           .addClass("hidden")
-                        .end()
-                        .find(".package-select")
-                            .removeClass("hidden")
-                        .end()
-                        .find(".amount-input")
-                            .removeClass("hidden")
-                        .end();
 
-                    cartItem
-                        .find(".edit-button")
-                           .addClass("hidden")
-                        .end()
-                        .find(".done-button")
-                            .removeClass("hidden")
-                        .end();
-
-                    updateEntryPackageTypeOptions(inputDiv.find(".package-select"), item.ProductID, inputDiv)
-                })
-                .text("Edit")
-            )
-            .append($("<button/>")
-                .addClass("btn btn-default pull-right hidden done-button")
-                .click(function() {
-                    inputDiv
-                        .find(".package-text")
-                           .removeClass("hidden")
-                        .end()
-                        .find(".amount-text")
-                           .removeClass("hidden")
-                        .end()
-                        .find(".package-select")
-                            .addClass("hidden")
-                        .end()
-                        .find(".amount-input")
-                            .addClass("hidden")
-                        .end();
-
-                    cartItem
-                        .find(".edit-button")
-                           .removeClass("hidden")
-                        .end()
-                        .find(".done-button")
-                            .addClass("hidden")
-                        .end();
-
-                        handleDirtyInput(inputDiv);
-                })
-                .text("Done")
-            )
-        )
-
-    return cartItem;
+    return cartItemEntry;
 }
 
-function handleDirtyInput(input) {
-    if (!input.data("dirty")) return;
+function handleDirtyInput(cartItem) {
+    var refresh = false;
+
+    cartItem.find(".item-input").each(function() {
+        if ($(this).data("dirty")) {
+            console.log("Dirty: Package: " + $(this).find(".package-select option:selected").text() + ", Amount: " + $(this).find(".amount-input").val());
+            refresh = true;
+        }
+    });
 
     // TODO post to route to update item
-    console.log("Input is dirty.");
 
-    displayCartInventory(); //refresh after done calling route
+    if (refresh) displayCartInventory(); //refresh after done calling route
 }
 
 //Updates the options for the package types of a single entry.
-function updateEntryPackageTypeOptions(entry, productId, input) {
-    var packageTypes = null;
-    entry.empty();
-
+function updateEntryPackageTypeOptions(input, productId) {
     $.get(window.apiRoute + "/GetSizeByProductID/" + productId, function(res) {
         if (res && res.length) {
-            packageTypes = $.parseJSON(res);
+            var packageTypes = $.parseJSON(res);
 
-            packageTypes.forEach(function(each) {
-                $("<option/>")
-                    .text(each.Name + " of " + each.Size)
-                    .attr("name", each.Name)
-                    .data("size", each.Size)
-                    .appendTo(entry);
+            input.each(function() {
+                var select = $(this).find(".package-select").empty();
+
+                packageTypes.forEach(function(each) {
+                    $("<option/>")
+                        .text(each.Name + " of " + each.Size)
+                        .attr("name", each.Name)
+                        .data("size", each.Size)
+                        .appendTo(select);
+                });
+
+                // Select the correct package type
+                var name = $(this).find(".package-text").attr("name");
+
+                select.find("option[name='" + name + "']").prop("selected", true);
             });
-
-            // Select the correct package type
-            var name = input.find(".package-text").attr("name");
-
-            input.find(".package-select option[name='" + name + "']").prop("selected", true);
         } else {
             $("#response").text("Error: Update package types: No response.");
         }
@@ -347,4 +367,35 @@ function updateTotal(cartItem) {
     });
 
     cartItem.find(".item-total-text").text(total);
+}
+
+function showPrototypeButtons() {
+    var cartContainer = $("#inventory-container")
+        .empty();
+
+    if ($("#selectDropDown :selected").val() == -1) return;
+
+    state.nameSelected = $("#selectDropDown :selected").text();
+    navigation.saveState(state);
+    var idSelected = $("#selectDropDown :selected").val();
+
+    $.get(window.apiRoute + "/Carts/GetCartItems/" + idSelected, function(res) {
+        if (res && res.length) {
+            var items = JSON.parse(res)[0];
+            populateCartContainer(items);
+
+            $("#inventory-container").find(".edit-button, .delete-button").each(function() {
+                $(this).removeClass("hidden")
+            });
+        }
+        else {
+            $("#response").text("Error: displayCartInventory: No response.");
+        }
+    }).fail(function(res) {
+        $("#response").text("Error: displayCartInventory: Connection error.");
+    });
+}
+
+function hidePrototypeButtons() {
+    displayCartInventory();
 }
