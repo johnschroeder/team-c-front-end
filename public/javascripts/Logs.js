f4b6a2647d2b6dc5a34e4b8c2f22714d598df470
Trevor <rtrevor@pdx.edu>
2015-07-07 16:45:24 -0700

    forward merge, comitting changes

--- public/javascripts/Logs.js
+++ public/javascripts/Logs.js
@@ -1,14 +1,7 @@
-/**
- * Created by Trevor on 7/3/2015.
- */
-/**
- * Add another row to the pull menu through cloning
- */
-
-var counter = 0; // I know, I know
+var counter = 0;
 function DisplayNext(log) {
 
-    var rowToCopy = $('.InputChild').first(); //Grab the first InputChild row to duplicate
+    var rowToCopy = $('.InputChild').first();
     var rowsContainer = '#InputDiv';
 
     var clonedRow = rowToCopy.clone();
@@ -26,15 +19,15 @@ var DisplayAll =
 {
     Now: function () {
         var host = window.apiRoute + "/getLogs/";
-        // alert(host);
+
             $.get(host, function (logsForUsername) {
                 var logsObj = JSON.parse(logsForUsername);
                 var logs = logsObj.logs;
 
                 for (var i = 0; i < logs.length; i++) {
+
                     var log = logs[i];
-                    //alert(log);
-                    DisplayNext(logs[i]);
+                    DisplayNext(log);
                 }
 
             });
