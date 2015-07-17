8aad3ccf1909f27d9686aae087187450faf388e4
kunmmckeighan <kunmckeighan@gmail.com>
2015-07-16 20:57:59 -0700

    IM-Cart-Kun-undrun Starting Point

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -2,14 +2,14 @@ var navigation = {
     stateTable:{},
     go:function(targetPage, args) {
         $("#main_cont").load('/load/' + targetPage, {args:args, state:this.stateTable[targetPage.toLowerCase().split(".")[0]]});
-        jQuery.get(window.apiRoute+"/getUserInfo", function(result){
+        /*jQuery.get(window.apiRoute+"/getUserInfo", function(result){
             jQuery('#loggedIn').toggle(true);
             jQuery('#login').toggle(false);
             jQuery('#usersName').text(function(){return result.FirstName+" "+result.LastName});
         }).fail(function(){
             jQuery('#login').toggle(true);
             jQuery('#loggedIn').toggle(false);
-        });
+        });*/
     },
     saveState:function(state) {
         this.stateTable[window.thisPage.toLowerCase().split(".")[0]] = state;
