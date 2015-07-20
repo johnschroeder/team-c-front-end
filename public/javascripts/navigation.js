ce82e8c3608541ae5b6cc23d64809b1792c127b9
elijah <emv2@pdx.edu>
2015-07-20 14:01:54 -0700

    Editing navigation.go to block unauthorized users from even viewing the html.... not done yet....

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -7,19 +7,10 @@ var navigation = {
             jQuery('#usersName').text(function(){return result.FirstName+" "+result.LastName});
         }).then(function(result){
             jQuery.get(window.apiRoute+ "/checkPermissions/" + targetPage + "/" + result.PermsID,function(res){
-                console.log(res);
-
-                if(res !== "Success"){
-                    console.log("denied");
-                    $("#main_cont").load('/load/' + "Home.html");
-                }
-                else{
-                    console.log("went through");
-                    $("#main_cont").load('/load/' + targetPage, {args:args, state:this.stateTable[targetPage.toLowerCase().split(".")[0]]});
-                    console.log(res);
-                }
-
-            })
+                $("#main_cont").load('/load/' + targetPage, {args:args, state:navigation.stateTable[targetPage.toLowerCase().split(".")[0]]});
+            }).fail(function(res){
+                $("#main_cont").load('/load/' + "Home.html");
+            });
         }).fail(function(){
             jQuery('#login').toggle(true);
             jQuery('#loggedIn').toggle(false);
@@ -34,8 +25,8 @@ var navigation = {
         }).fail(function(res){
             if(res.status == 511){
                 console.log("Access Denied!");
-                alert("Sorry your permission level doesn't allow you to access this page.");
-                navigation.go("Home.html");
+                //alert("Sorry your permission level doesn't allow you to access this page.");
+               // navigation.go("Home.html");
             }
             if(res.status == 510){
                 navigation.go("loginForm.html");
