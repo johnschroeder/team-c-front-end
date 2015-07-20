8b0fba2c1196e3236f02094864bf5f4cafc3816a
elijah <emv2@pdx.edu>
2015-07-20 11:36:12 -0700

    Editing navigation.go to block unauthorized users from even viewing the html.... not done yet....

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -1,11 +1,25 @@
 var navigation = {
     stateTable:{},
     go:function(targetPage, args) {
-        $("#main_cont").load('/load/' + targetPage, {args:args, state:this.stateTable[targetPage.toLowerCase().split(".")[0]]});
         jQuery.get(window.apiRoute+"/getUserInfo", function(result){
             jQuery('#loggedIn').toggle(true);
             jQuery('#login').toggle(false);
             jQuery('#usersName').text(function(){return result.FirstName+" "+result.LastName});
+        }).then(function(result){
+            jQuery.get(window.apiRoute+ "/checkPermissions/" + targetPage + "/" + result.PermsID,function(res){
+                console.log(res);
+
+                if(res !== "Success"){
+                    console.log("denied");
+                    $("#main_cont").load('/load/' + "Home.html");
+                }
+                else{
+                    console.log("went through");
+                    $("#main_cont").load('/load/' + targetPage, {args:args, state:this.stateTable[targetPage.toLowerCase().split(".")[0]]});
+                    console.log(res);
+                }
+
+            })
         }).fail(function(){
             jQuery('#login').toggle(true);
             jQuery('#loggedIn').toggle(false);
