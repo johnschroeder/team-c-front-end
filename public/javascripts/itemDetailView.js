9ce8d77fbaf1be9c347d245b2c02bc2596606c07
TimSwart <tswart@pdx.edu>
2015-07-28 00:22:06 -0700

    changed formatting, not ready for mobile or small screens

--- public/javascripts/itemDetailView.js
+++ public/javascripts/itemDetailView.js
@@ -49,10 +49,13 @@ var itemDetailView = {
     displayRuns: function () {
         //this.runs.forEach(function (run) {
         // using for loop instead of forEach to reverse order
+        $("#runs").append("<div class=\"col-sm-12\">" + "<b>" + "<div class=\"col-sm-1\">" + "Run ID" + "</div>" + "<div class=\"col-sm-1\">" + "Alt ID" + "</div>" + "<div class=\"col-sm-2\">" + "Location" + "</div>" + "<div class=\"col-sm-2\">" + "Date Created" + "</div>" + "<div class=\"col-sm-2\">" + "Initial Quantity" + "</div>" + "<div class=\"col-sm-2\">" + "Quantity Available" + "</div>" + "<div class=\"col-sm-2\">" + "Quantity Reserved" + "</div>" + "</b>" + "</div>");
         for (var i = this.runs.length - 1; i > 0; --i) {
             var run = this.runs[i];
             var dateRegex = /\d\d\d\d-\d\d-\d\d/;
-            $("#runs").append("<div>" + "RunID:" + run.RunID + "Location:" + run.Location + "DateCreated:" + dateRegex.exec(run.DateCreated) + "<br/>" + "InitialQuantity:" + run.InitialQuantity + "QuantityAvailable:" + run.QuantityAvailable + "QuantityReserved:" + run.QuantityReserved + "<br/><br/>" + "</div>");
+            //$("#runs").append("<div>" + "RunID: " + run.RunID + "&nbsp &nbsp" + " Location: " + run.Location + "&nbsp &nbsp" + " DateCreated: " + dateRegex.exec(run.DateCreated) + "InitialQuantity: " + run.InitialQuantity + "&nbsp &nbsp" + " QuantityAvailable: " + run.QuantityAvailable + "&nbsp &nbsp" + " QuantityReserved: " + run.QuantityReserved + "<br/><br/>" + "</div>");
+            $("#runs").append("<div class=\"col-sm-12\">" + "<div class=\"col-sm-1\">" + run.RunID + "</div>" + "<div class=\"col-sm-1\">" + run.AltID + "</div>" + "<div class=\"col-sm-2\">" + run.Location + "</div>" + "<div class=\"col-sm-2\">" + dateRegex.exec(run.DateCreated) + "</div>" + "<div class=\"col-sm-2\">" + run.InitialQuantity + "</div>" + "<div class=\"col-sm-2\">" + run.QuantityAvailable + "</div>" + "<div class=\"col-sm-2\">" + run.QuantityReserved + "</div>" + " </div>");
+
         }
     },
 
